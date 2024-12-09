632d2ea1d3978707e65dcf4c5f31df4128cb05d8
Sindhur V Shabaraya <sindhur.shabaraya@contentstack.com>
2024-12-09 17:11:43 +0530

    fix cyclic highlight using visited nodes

--- src/containers/Editor/components/views/GraphView/CustomEdge/index.tsx
+++ src/containers/Editor/components/views/GraphView/CustomEdge/index.tsx
@@ -7,8 +7,8 @@ const CustomEdgeWrapper = (props: EdgeProps) => {
   const { highlighedPaths, highlightedNodes } = useHighlight();
 
   return <Edge containerClassName={`edge-${props.id}`} {...props} style={{
-    stroke: highlighedPaths.has(props.id.slice(12)) ? "darkblue" : !(highlighedPaths.size === 0) && "gray",
-    opacity: highlighedPaths.has(props.id.slice(12)) || ((highlighedPaths.size === 0) && highlightedNodes.size === 0) ? 1 : 0.2
+    stroke: highlighedPaths.has(props.id.slice(11)) ? "darkblue" : !(highlighedPaths.size === 0) && "gray",
+    opacity: highlighedPaths.has(props.id.slice(11)) || ((highlighedPaths.size === 0) && highlightedNodes.size === 0) ? 1 : 0.2
   }}/>;
 };
 
